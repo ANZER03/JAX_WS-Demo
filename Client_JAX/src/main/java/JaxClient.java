38c72f5b08ce04436db8401c8b3015fb5e6e6d58
AnouarZerrik <an_zerrik@etu.enset-media.ac.ma>
2024-12-19 23:35:11 +0000

    Add Jax Client proxy 2

---
+++ Client_JAX/src/main/java/JaxClient.java
@@ -0,0 +1,18 @@
+import proxy.BanqueService;
+import proxy.BanqueServiceService;
+import proxy.Compte;
+
+import java.util.List;
+
+public class JaxClient {
+    public static void main(String[] args) {
+
+        BanqueService stub = new BanqueServiceService().getBanqueServicePort();
+        List<Compte> comptes = stub.getComptes();
+
+        comptes.stream().forEach(compte -> {
+            System.out.println(compte.getId() + " | " + compte.getSolde() + " | " + compte.getDate());
+        });
+    }
+
+    }
---
+++ Client_JAX/src/main/java/proxy/Compte.java
@@ -0,0 +1,117 @@
+
+package proxy;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlSchemaType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour compte complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="compte"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="date" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/&gt;
+ *         &lt;element name="solde" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "compte", propOrder = {
+    "date",
+    "id",
+    "solde"
+})
+public class Compte {
+
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar date;
+    protected Integer id;
+    protected Double solde;
+
+    /**
+     * Obtient la valeur de la propriété date.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDate() {
+        return date;
+    }
+
+    /**
+     * Définit la valeur de la propriété date.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDate(XMLGregorianCalendar value) {
+        this.date = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété id.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Définit la valeur de la propriété id.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setId(Integer value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété solde.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getSolde() {
+        return solde;
+    }
+
+    /**
+     * Définit la valeur de la propriété solde.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setSolde(Double value) {
+        this.solde = value;
+    }
+
+}
